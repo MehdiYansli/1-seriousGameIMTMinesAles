5208cdab4ff7d5c1cdaf1d81d9db2e30925cc62e
MehdiYsl <mehdi.yansli@gmail.com>
2023-04-24 15:49:58 +0200

    rajout du fichier index.php

--- src/Controller/PersonnagesController.php
+++ src/Controller/PersonnagesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\PersonnagesRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class PersonnagesController extends AbstractController
 {
     #[Route('/personnages', name: 'app_personnages')]
-    public function index(): JsonResponse
+    public function index(EntityManagerInterface $entityManager): JsonResponse
     {
+        $personnages = $entityManager->getRepository(PersonnagesRepository::class)->findAll();
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/PersonnagesController.php',
