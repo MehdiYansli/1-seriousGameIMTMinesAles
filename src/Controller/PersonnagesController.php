7e6f8b25eb641a9471f7272cbd148f72671b7c77
MehdiYsl <mehdi.yansli@gmail.com>
2023-04-25 10:02:49 +0200

    Changement de repository name par entity name dans le controller personnages pour palier à l'erreur

--- src/Controller/PersonnagesController.php
+++ src/Controller/PersonnagesController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
-use App\Repository\PersonnagesRepository;
-use Doctrine\ORM\EntityManager;
+use App\Entity\Personnages;
+// use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\PersonnagesRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PersonnagesController extends AbstractController
 {
     #[Route('/personnages', name: 'app_personnages')]
     public function index(EntityManagerInterface $entityManager): JsonResponse
     {
-        $personnages = $entityManager->getRepository(PersonnagesRepository::class)->findAll();
+        $personnages = $entityManager->getRepository(Personnages::class)->findAll();
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
