4c782af02f66bdf53c2f961550da59934e3b5676
MehdiYsl <mehdi.yansli@gmail.com>
2023-05-03 10:32:23 +0200

    Réglage du problème d'affichage easyadmin en re installant le design et changement de la zone de texte

--- src/Controller/Admin/PersonnagesCrudController.php
+++ src/Controller/Admin/PersonnagesCrudController.php
@@ -5,9 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Personnages;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PersonnagesCrudController extends AbstractCrudController
@@ -25,7 +24,7 @@ class PersonnagesCrudController extends AbstractCrudController
             IntegerField::new('positionX')->setLabel('Position en X'),
             IntegerField::new('positionY')->setLabel('Position en Y'),
             TextField::new('name')->setLabel('Nom du personnage'),
-            TextEditorField::new('text')->setLabel('Texte du personnage'),
+            TextareaField::new('text')->setLabel('Texte du personnage'),
         ];
     }
     
